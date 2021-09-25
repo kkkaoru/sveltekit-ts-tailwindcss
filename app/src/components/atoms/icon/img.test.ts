42c967fa38728babbd683d5c336e314651f89536
kaoru.ohtaka <ockacru+git@gmail.com>
2021-09-26 04:08:54 +0900

    Add test code in components

---
+++ app/src/components/atoms/icon/img.test.ts
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/svelte';
+import { mockEmojiThumbsUpSvgUrl } from '../../../mocks/emoji/thumbs-up';
+import Icon from './img.svelte';
+
+describe('shows proper heading when rendered', () => {
+  it('should be matched snapshot', () => {
+    const { container } = render(Icon, {
+      src: mockEmojiThumbsUpSvgUrl,
+      alt: 'test',
+      width: 128,
+      height: 128,
+    });
+    expect(container).toMatchSnapshot();
+  });
+});
